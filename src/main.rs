eee733c24c6b76d51457b2f9e1c0e0c07ca957f3
Дамјан Георгиевски <gdamjan@gmail.com>
2020-04-12 01:17:11 +0200

    listen to unix-domain socket too
    vm guests connecting to the host port will be redirected to this socket by
    cloud-hypervisor/firecracker.
    
    see https://github.com/firecracker-microvm/firecracker/blob/master/docs/vsock.md

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 use async_std::net::{TcpStream, TcpListener};
+use async_std::os::unix::net::{UnixStream, UnixListener};
 use async_std::prelude::*;
 use async_std::task;
 use http_types::{Response, StatusCode};
 
 const HTTP_PORT: u16 = 8888;
 const HTTP_ADDR: &str = "127.0.0.1";
+const VSOCK_PORT: u32 = 12345;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
-    task::spawn(http_listener(HTTP_ADDR, HTTP_PORT)).await?;
+    let h = task::spawn(http_listener(HTTP_ADDR, HTTP_PORT));
+    let v = task::spawn(vsock_listener("/tmp/micro-vm.sock", VSOCK_PORT));
+
+    h.await?;
+    v.await?;
     Ok(())
 }
 
+
+async fn vsock_listener(addr: &str, port: u32) -> std::io::Result<()> {
+    let addr = format!("{}_{}", addr, port);
+    let listener = UnixListener::bind(&addr).await?;
+    let mut incoming = listener.incoming();
+
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        task::spawn(async move {
+            if let Err(err) = handle_subscription(stream).await {
+                eprintln!("{}", err);
+            }
+        });
+    }
+    Ok(())
+}
+
+async fn handle_subscription(_stream: UnixStream) -> std::io::Result<()> {
+    Ok(())
+}
 
 async fn http_listener(addr: &str, port: u16) -> std::io::Result<()> {
     // Open up a TCP connection and create a URL.
