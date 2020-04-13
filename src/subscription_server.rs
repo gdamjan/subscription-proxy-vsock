66e70a5c6c0326681599f0e6c53842b3ef167144
Дамјан Георгиевски <gdamjan@gmail.com>
2020-04-14 00:30:46 +0200

    properly parse the port from a REGISTER <port>\n command

--- src/subscription_server.rs
+++ src/subscription_server.rs
@@ -1,9 +1,10 @@
 use async_std::os::unix::net::{UnixListener, UnixStream};
 use async_std::prelude::*;
-use async_std::task;
+use async_std::{future, task};
+use std::io;
 use std::time::Duration;
 
-pub async fn subscription_listener(vm_sock_addr: &str, port: u32) -> std::io::Result<()> {
+pub async fn subscription_listener(vm_sock_addr: &str, port: u32) -> io::Result<()> {
     let listen_addr = format!("{}_{}", vm_sock_addr, port);
     let listener = UnixListener::bind(&listen_addr).await?;
     let mut incoming = listener.incoming();
@@ -20,8 +21,9 @@ pub async fn subscription_listener(vm_sock_addr: &str, port: u32) -> std::io::Re
     Ok(())
 }
 
-async fn handle_subscription(addr: &str, mut stream: UnixStream) -> std::io::Result<()> {
+async fn handle_subscription(addr: &str, mut stream: UnixStream) -> io::Result<()> {
     // read lines of the form "REGISTER <port>\n"
+    const REGISTER: &[u8] = b"REGISTER ";
 
     let mut register_request = Vec::<u8>::new();
     // poor mans take_while
@@ -31,24 +33,39 @@ async fn handle_subscription(addr: &str, mut stream: UnixStream) -> std::io::Res
         register_request.push(single_byte[0]);
         single_byte != [b'\n']
     } {}
-    print!("Subscriber: {}:{}", addr, String::from_utf8_lossy(&register_request));
+
+    let port: u32 = if register_request.starts_with(REGISTER) && register_request.ends_with(b"\n") {
+        let start = REGISTER.len();
+        let end = register_request.len() - 1;
+        let s = String::from_utf8_lossy(&register_request[start..end]);
+        s.parse::<u32>()
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "Bad REGISTER request"))?
+    } else {
+        return Err(io::Error::new(io::ErrorKind::Other, "Bad REGISTER request"));
+    };
+    println!("Registered: {}:{}", addr, port);
 
     /* FIXME
-        parse the register command, get the port as u32,
         add (addr, port) to a "subscribers" structure
-
-        let mut iter = buffer.split_ascii_whitespace();
-        if let Some("REGISTER") = iter.next() {
-
-        }
         crate::SUBSCRIBERS.lock().await.push_back(…);
     */
 
     loop {
+        let mut response: Vec<u8> = vec![0; 5];
         stream.write_all(b"ping\n").await?;
-        // FIXME: check for pongs, if they fail remove it from subscribers
+        if future::timeout(Duration::from_secs(5), stream.read_exact(&mut response))
+            .await
+            .is_err()
+        {
+            break;
+        }
+        if response != b"pong\n" {
+            break;
+        }
         task::sleep(Duration::from_secs(5)).await;
     }
+    // FIXME: deregister
+    println!("De-registered: {}:{}", addr, port);
 
     Ok(())
 }
