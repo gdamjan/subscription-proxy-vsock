9c78d0c81e72bfeb91ead9532c85f6b6be51fabf
Дамјан Георгиевски <gdamjan@gmail.com>
2020-04-14 19:18:09 +0200

    use the low level httparse crate instead of async-h1
    removed async_h1::accept and async_h1::client::connect, and parse the
    request with httparse
    
    WIP: builds and runs, probably not very correct

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,8 +1,8 @@
+use async_std::io;
 use async_std::net::{TcpListener, TcpStream};
 use async_std::os::unix::net::UnixStream;
 use async_std::prelude::*;
 use async_std::task;
-use http_types::{Response, StatusCode};
 
 pub async fn http_listener(addr: &str, port: u16) -> std::io::Result<()> {
     let listener = TcpListener::bind((addr, port)).await?;
@@ -12,10 +12,10 @@ pub async fn http_listener(addr: &str, port: u16) -> std::io::Result<()> {
     // For each incoming TCP connection, spawn a task to handle it
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
-        let stream = stream?;
+        let mut stream = stream?;
         let addr = addr.clone();
         task::spawn(async move {
-            if let Err(err) = handle_http_request(&addr, stream).await {
+            if let Err(err) = handle_http_request(&addr, &mut stream).await {
                 eprintln!("{}", err);
             }
         });
@@ -23,55 +23,68 @@ pub async fn http_listener(addr: &str, port: u16) -> std::io::Result<()> {
     Ok(())
 }
 
-async fn handle_http_request(addr: &str, stream: TcpStream) -> http_types::Result<()> {
+async fn handle_http_request(_addr: &str, stream: &mut TcpStream) -> io::Result<()> {
     println!("Got request from {}", stream.peer_addr()?);
-    async_h1::accept(addr, stream.clone(), |req| async move {
-        let (addr, port) = match crate::SUBSCRIBERS.get().await {
-            Some(t) => t,
-            None => return bad_gateway(),
-        };
 
-        let mut vsock_stream = match UnixStream::connect(addr).await {
-            Ok(v) => v,
-            Err(_) => return bad_gateway(),
-        };
+    let mut headers = [httparse::EMPTY_HEADER; 16]; // FIXME: more than 16 headers?
+    let mut req = httparse::Request::new(&mut headers);
 
-        let connect_cmd = format!("CONNECT {}\n", port);
-        if vsock_stream
-            .write_all(connect_cmd.as_bytes())
-            .await
-            .is_err()
-        {
-            return bad_gateway();
-        }
+    let bufsize: usize = 64 * 1024; // FIXME: is 64kb enough
+    let mut buf = vec![0; bufsize];
+    //
+    let len = stream.read(&mut buf).await?; // FIXME: how much is read, is it enough?
+    eprintln!("{}", len);
+    let res = req
+        .parse(&buf)
+        .map_err(|_| io::Error::new(io::ErrorKind::Other, "parsing failed"))?;
 
-        // poor mans take_while
-        let mut connect_response = Vec::<u8>::new();
-        while {
-            let mut single_byte = vec![0; 1];
-            if vsock_stream.read_exact(&mut single_byte).await.is_err() {
-                return bad_gateway();
-            }
-            connect_response.push(single_byte[0]);
-            single_byte != [b'\n']
-        } {}
+    if res.is_partial() && req.path.is_none() {
+        return Err(io::Error::new(io::ErrorKind::Other, "parsing partial"));
+    };
 
-        if !connect_response.starts_with(b"OK ") {
-            return bad_gateway();
-        }
+    // FIXME: routing here: req.path
+    let mut vsock = get_subscriber().await?;
+    // vsock.write_all(... TODO ...).await?; // send parsed headers
+    // vsock.write_all(&buf[body_idx..]).await?;
+    vsock.write_all(&buf).await?;
 
-        // FIXME: double headers in request and response
-        let res = async_h1::client::connect(vsock_stream, req).await?;
-        Ok(res)
-    })
-    .await?;
+
+    // IDEALLY use splice(2) here!
+
+    // FIXME: this blocks until the end of the request stream - which is not what we need,
+    // and other problems (second request on the same connection etc…)
+    // io::copy(stream, &mut vsock).await?; // send the rest of stream to vsock
+
+
+    // now copy the response to the client
+    io::copy(&mut vsock, stream).await?;
+
     Ok(())
 }
 
-fn bad_gateway() -> http_types::Result<Response> {
-    // Err(http_types::Error::from_str(StatusCode::BadGateway, "Bad Gateway")
-    let mut res = Response::new(StatusCode::BadGateway);
-    res.insert_header("Content-Type", "text/plain")?;
-    res.set_body("Bad Gateway");
-    Ok(res)
+async fn get_subscriber() -> io::Result<UnixStream> {
+    let (addr, port) = crate::SUBSCRIBERS
+        .get()
+        .await
+        .ok_or(io::Error::new(io::ErrorKind::Other, "no subscribers"))?;
+
+    let mut vsock_stream = UnixStream::connect(addr).await?;
+
+    let connect_cmd = format!("CONNECT {}\n", port);
+    vsock_stream.write_all(connect_cmd.as_bytes()).await?;
+
+    // poor mans take_while
+    let mut connect_response = Vec::<u8>::new();
+    while {
+        let mut single_byte = vec![0; 1];
+        vsock_stream.read_exact(&mut single_byte).await?;
+        connect_response.push(single_byte[0]);
+        single_byte != [b'\n']
+    } {}
+
+    if !connect_response.starts_with(b"OK ") {
+        return Err(io::Error::new(io::ErrorKind::Other, "connect failed"));
+    }
+
+    Ok(vsock_stream)
 }
